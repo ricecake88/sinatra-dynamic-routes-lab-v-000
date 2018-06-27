ff03016475af7610c2c9b65224cf0a9f205424b4
Grace Shih <vocalise@gmail.com>
2018-06-27 05:15:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     phrase = params[:phrase]
     num = params[:number].to_i
-  10.times {puts "#{phrase}"}
+  10.times {|i| puts "#{phrase}"}
   end
   
 end
