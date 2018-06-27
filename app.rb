cc9510add548bb51699038bf11d8f5641736c7e8
Grace Shih <vocalise@gmail.com>
2018-06-27 05:32:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ class App < Sinatra::Base
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
     op = params[:operation]
-    if op == "add"
+    case 'add':
       result = num1 + num2
       result.to_s
     elsif op == "subtract"
